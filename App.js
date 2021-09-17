5cd1c4ac8c47a92d1cb371779c5f0a54d7b3b57f
Discoonect <67423146+Discoonect@users.noreply.github.com>
2021-09-17 21:50:10 +0900

    permission 및 위도경도 추가 -> 날씨 api 추가해야함

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
 export default class extends React.Component {
-  getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+
+  state = {
+    isLoading : true
   }
+
+  getLocation = async() => {
   
-  componentDidMount(){
+    try{
+      const response = await Location.requestForegroundPermissionsAsync();
+      
+      const {
+        coords : {latitude, longitude}
+        } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading : false });
+      
+    } catch (error) {
+      Alert.alert("당신을 찾을 수 없어요","너무 슬퍼요");
+      
+    }
+    
+  }
+
+  componentDidMount() {
     this.getLocation();
   }
 
-  render(){
-    return <Loading/>;
-  }  
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
+  }
+
+  
+
+  
+
+  
 }
