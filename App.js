66f1833a529315d1c5d1e32a693420a424257984
Discoonect <67423146+Discoonect@users.noreply.github.com>
2021-09-21 04:02:32 +0900

    openWeatherAPI 연결 ok

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
+import axios from "axios"
+import Weather from "./Weather"
+
+const API_KEY = "bd79a62f3d0f41fb801fdbdf4063c0e9";
 
 export default class extends React.Component {
 
@@ -9,35 +13,30 @@ export default class extends React.Component {
     isLoading : true
   }
 
-  getLocation = async() => {
-  
-    try{
-      const response = await Location.requestForegroundPermissionsAsync();
-      
-      const {
-        coords : {latitude, longitude}
-        } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading : false });
-      
-    } catch (error) {
-      Alert.alert("당신을 찾을 수 없어요","너무 슬퍼요");
-      
-    }
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    this.setState({ isLoading : false, temp : data.main.temp })
     
   }
+
+  getLocation = async () => {
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords : {latitude, longitude}
+        } = await Location.getCurrentPositionAsync();  
+        this.getWeather(latitude, longitude)
+    } catch (error) {
+      Alert.alert("당신을 찾을 수 없어요","너무 슬퍼요"); 
+    }
+  };
 
   componentDidMount() {
     this.getLocation();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading/> : null;
+    const { isLoading, temp } =  this.state;
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)}/>;
   }
-
-  
-
-  
-
-  
 }
